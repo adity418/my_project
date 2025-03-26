c61fe6f70a685f4be0ef1884c5a2e857450a5153
aditya-dev <adity119@gmail.com>
2025-03-26 11:27:44 +0530

    Update todo_app/urls.py

--- to_do_list/todo_app/urls.py
+++ to_do_list/todo_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path("", views.TodoListView.as_view(), name="index"),
+    path("list/<int:llist_id>/", views.ItemListView.as_view(), name="list"),
+    path("list/add/", views.ToDoCreate.as_view(), name="list-add"),
+    path("list/<int:list_id>/item/add/", views.ItemCreate.as_view(), name="item-add"),
+    path("list/<int:list_id/item/<int:pk>", views.ItemUpdate.as_view(), name="item-update"),
+    path("list/<int:pk>/delete/", views.ToDoDelete.as_view(), name="list-delete"),
+    path("list/<int:list_id>/item/<int:pk>/delete/", views.ItemDelete.as_view(), name="item-delete"),
 ]
