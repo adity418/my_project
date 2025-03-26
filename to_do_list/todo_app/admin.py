c841782d60039ca132a082357158fd873944badb
aditya-dev <adity119@gmail.com>
2025-03-26 18:02:13 +0530

    Update changes

--- to_do_list/todo_app/admin.py
+++ to_do_list/todo_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import ToDoItem,ToDoList
+from todo_app.models import ToDoItem,ToDoList
 
 # Register your models here.
 
--- to_do_list/todo_app/urls.py
+++ to_do_list/todo_app/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.TodoListView.as_view(), name="index"),
-    path("list/<int:llist_id>/", views.ItemListView.as_view(), name="list"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
     path("list/add/", views.ToDoCreate.as_view(), name="list-add"),
     path("list/<int:list_id>/item/add/", views.ItemCreate.as_view(), name="item-add"),
-    path("list/<int:list_id/item/<int:pk>", views.ItemUpdate.as_view(), name="item-update"),
+    path("list/<int:list_id>/item/<int:pk>/", views.ItemUpdate.as_view(), name="item-update"),
     path("list/<int:pk>/delete/", views.ToDoDelete.as_view(), name="list-delete"),
     path("list/<int:list_id>/item/<int:pk>/delete/", views.ItemDelete.as_view(), name="item-delete"),
 ]
